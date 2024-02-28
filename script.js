eee5dc3b62ca191a4a420f0690ca449b20d6dcf1
uzairahmed987 <156950220+uzairahmed987@users.noreply.github.com>
2024-02-28 09:39:42 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+let user={}
+console.log(user)
+let UserDesignation=prompt("Enter your designation:")
+user.User_Designation=UserDesignation;
+let IdNumber = prompt("Enter your ID number:")
+user.IdNumber=IdNumber;
+alert("Thanks for Providing information");
+
+
